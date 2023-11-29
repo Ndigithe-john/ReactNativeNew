13b79bef8cf8b2a9031ada075a3dae766ac067c2
Ndigithe-john <ndigithejohn@gmail.com>
2023-11-29 15:22:07 -0800

    added a navigators application to use drawer navigators and other React Native navigators

---
+++ ReactNaviteNavigators/App.js
@@ -0,0 +1,41 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigation } from "@react-navigation/drawer";
+import { WelcomeScreen } from "./screens/WelcomeScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { UserScreen } from "./screens/UserScreen";
+const Drawer = createDrawerNavigation();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActiveTintcolor: "#3c016b",
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          drawerStyle: { backgroundColor: "#ccc" },
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screeen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
