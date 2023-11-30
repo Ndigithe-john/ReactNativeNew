28e07cbc43a38ffa739dab6739da10c17032a113
Ndigithe-john <ndigithejohn@gmail.com>
2023-11-30 13:33:00 -0800

    added nested navigation

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -1,18 +1,53 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import CategoryScreens from "./screens/CategoryScreens";
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="MealsCategories" component={CategoryScreens} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#351401",
+        },
+        headerTintColor: "white",
+        sceneContainerStyle: {
+          backgroundColor: "#3f2f25",
+        },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="MealsCategories"
+        component={CategoryScreens}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -35,11 +70,9 @@ export default function App() {
           <Stack.Screen
             name="DrawerScreen"
             component={DrawerNavigator}
-            options={
-              {
-                // title: "All Categories", no longer needed in nesting
-              }
-            }
+            options={{
+              headerShown: false,
+            }}
           />
           <Stack.Screen name="Meals" component={MealsOverviewScreen} />
           <Stack.Screen name="MealDetails" component={MealDetailsScreen} />
